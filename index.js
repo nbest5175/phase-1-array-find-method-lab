01a0f727c2cf53e0af9b1bfca6061a30b3d0b47f
Nicholas <bestnicholas05@gmail.com>
2024-05-12 18:27:08 -0400

    Completed Phase 1 Array Find Method Lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+
+function superbowlWin(records) {
+    const res = records.find(record => record.result === "W")
+//    return res.year
+   if (res === undefined) {
+    return undefined
+   }
+console.log("This is our variable res: ", res)
+   if (res.result === "W")
+    return res.year
+}
+
